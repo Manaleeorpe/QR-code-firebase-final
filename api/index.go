8f302c234c9912e1fed9d3ea16b7b84bb25f7af1
Manaleeorpe <71582885+Manaleeorpe@users.noreply.github.com>
2025-08-01 14:27:49 +0530

    Update index.go

--- api/index.go
+++ api/index.go
@@ -1,21 +1,24 @@
 package handler
 
 import (
-    "net/http"
+	"net/http"
 
-    "github.com/gorilla/mux"
-    "qr-code-generator/pkg/routes"
+	"qr-code-generator/pkg/config"
+	"qr-code-generator/pkg/routes"
+
+	"github.com/gorilla/mux"
 )
 
 var router *mux.Router
 
 func init() {
-    router = mux.NewRouter()
-    routes.RegisterQRCodeGeneratorstoreRoutes(router)
-    routes.RegisterQRCodeGeneratorFirebasestoreRoutes(router)
+	config.ConnectFirebase()
+	router = mux.NewRouter()
+	routes.RegisterQRCodeGeneratorstoreRoutes(router)
+	routes.RegisterQRCodeGeneratorFirebasestoreRoutes(router)
 }
 
 // Handler is Vercel entry point
 func Handler(w http.ResponseWriter, r *http.Request) {
-    router.ServeHTTP(w, r)
+	router.ServeHTTP(w, r)
 }
