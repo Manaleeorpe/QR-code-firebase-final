60d48bd9ed71566cda56b24ccbad558c29f2ada5
Manaleeorpe <71582885+Manaleeorpe@users.noreply.github.com>
2025-08-01 12:46:18 +0530

    Update index.go

--- api/index.go
+++ api/index.go
@@ -4,6 +4,11 @@ import (
   "fmt"
   "net/http"
 )
+func init() {
+	// initialize your mux router using existing routes
+	//router = routes.SetupRoutes()
+	log.Println("Router initialized")
+}
  
 func Handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
